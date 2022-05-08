7c029612aee9a5620927aa44fe95a3ce6c839024
jcmonnet <jcmonnet0408@gmail.com>
2022-05-08 13:32:37 +0200

    light mode avec un js 10 fois plus simple et court. Et surtout home made!!

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-let toggle = document.getElementById("lightmode");
-
-let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
-
-
-toggle.onclick = function() {
-    let currentTheme = document.documentElement.getAttribute("data-theme");
-    let targetTheme = "dark";
-
-    if (currentTheme === "dark") {
-        targetTheme = "light";
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme);
+let couleur=document.querySelector('body');
+couleur.addEventListener('click',change);
+function change () {
+    couleur.classList.toggle("light");
 }
