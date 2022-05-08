39af7a9db37243ff74e759bd825097208dcf9f8f
jcmonnet <jcmonnet0408@gmail.com>
2022-05-08 14:12:23 +0200

    retrait classe body html car m'affichait la page en light , alors que je veux le dark par defaut

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let couleur=document.getElementById('lightmode');
-couleur.addEventListener('click',change);
-function change () {
-    let blanc=document.body
+// lightmode:
+// Après avoir dans le css, mis le dark sur mon body par defaut. J'ai mis toutes mes var mode clair dans la classe light.
+// Et dans le js, je pointe vers mon bouton, et lui demande au clic de lancer la fonction change.
+// La fonction change où je déclare la var blanc, qui pointe vers la classe du body. Et la fonction switch/toggle sur la classe light=mode clair
+let couleur = document.getElementById('lightmode');
+couleur.addEventListener('click', change);
+function change() {
+    let blanc = document.body
     blanc.classList.toggle("light");
 }
